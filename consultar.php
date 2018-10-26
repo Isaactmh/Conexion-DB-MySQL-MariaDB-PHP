4d431d8fc73de4be338457cb1d117543dad34afc
Isaac Rosario Duran <ic.elnene@gmail.com>
2018-10-26 19:59:38 -0400

    Update consultar.php

--- consultar.php
+++ consultar.php
@@ -1,17 +1,8 @@
 <?php
-
-class consultataller{
-
-    public $clienteid;
-
-    public function consultartaller()
-    {
-        $model = new Conexion;
-        $con = $model->conectar();
-        $sql = "SELECT cliente_id, nombre, telefono, servicio, precio, estado, tecnico FROM taller WHERE cliente_id=:cliente_id";
-        $stmt = $con->prepare($sql);
-        $stmt->bindParam(':cliente_id', $this->clienteid, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt;
-    }
-}
+        $sql = "SELECT cliente_id, nombre, telefono FROM clientes WHERE cliente_id=:cliente_id";
+        
+        $stmt = $conn->prepare($sql); //Preparamos la consulta
+        
+        $stmt->bindParam(':cliente_id', $this->clienteid, PDO::PARAM_INT); //Enviamos los datos de la consulta, en este caso el ID del cliente
+        
+        $stmt->execute(); //Ejecutamos nuestra consulta
